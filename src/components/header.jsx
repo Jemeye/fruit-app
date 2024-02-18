debfb0b93ad53276d7bea1926bf0702317c0fb0d
jemeye <jeni.medina2000@gmail.com>
2024-02-18 10:18:51 -0500

    signup button in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,23 @@
-// import { Link } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
-import { FaUser } from "react-icons/fa";
+import { FaSearch, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+
+    const navigate = useNavigate();
+    const isAuthenticated = localStorage.getItem('user');
+
+    // This function removes the user from the local storage and redirects to the login page
+    const logOut = (e) => {
+        e.preventDefault();
+        if (isAuthenticated) {
+            localStorage.removeItem('user');
+            navigate('/login');
+        } else {
+            navigate('/login');
+        }
+    }
+
 
     return (
         <header className="header-container">
@@ -13,7 +27,11 @@ function Header() {
             <nav className="nav-container">
                 <ul className="nav-list">
                     <li className="nav-item"><FaSearch></FaSearch></li>
-                    <button><li className="nav-item"><FaUser></FaUser></li></button>
+                    <button onClick={logOut}>
+                        <li className="nav-item">
+                            { isAuthenticated ? <FaSignOutAlt></FaSignOutAlt> : <FaSignInAlt></FaSignInAlt> }
+                        </li>
+                    </button>
                 </ul>
             </nav>
         </header>
