a07a41a6507d2917dec2f8a46c03b121866f32e5
jemeye <jeni.medina2000@gmail.com>
2024-02-18 09:46:44 -0500

    Configure react router

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,15 @@
 // import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Header() {
 
     return (
         <header className="header-container">
-            <div className="logo-container">
+            <Link className="logo-container" to="/">
                 <h1 className="logo">Fruit</h1>
-            </div>
+            </Link>
             <nav className="nav-container">
                 <ul className="nav-list">
                     <li className="nav-item"><FaSearch></FaSearch></li>
